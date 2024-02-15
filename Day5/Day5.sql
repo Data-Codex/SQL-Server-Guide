bc6588edbe6ccd8e39f1f793afeb9fce210a714e
Data-Codex <159218672+Data-Codex@users.noreply.github.com>
2024-02-15 22:03:06 +0530

    Foreign key reference, cascading, check Constraint

---
+++ Day5/Day5.sql
@@ -0,0 +1,34 @@
+
+----------------------------------------------------------------------------------------------------------------
+
+--DAY 5
+
+----------------------------------------------------------------------------------------------------------------
+
+--CREATE A TABLE
+
+CREATE TABLE Course
+(
+	CourseId INT PRIMARY KEY
+,	CourseName VARCHAR(255)
+);
+
+--CREATE AOTHER TABLE
+
+CREATE TABLE Student
+(
+	Id INT PRIMARY KEY
+,	Name VARCHAR(255)
+,	Age INT
+,	CourseId INT DEFAULT 1 FOREIGN KEY REFERENCES Course(CourseId)
+);
+
+--ADDING A CHECK CONSTRAINT 
+
+ALTER TABLE Student
+ADD CONSTRAINT CK_Student_Age CHECK (Age >=1 AND Age <=150) --or Age BETWEEN 1 AND 150
+
+--DROP A CHECK CONSTRAINT 
+
+ALTER TABLE Student
+DROP CONSTRAINT CK_Student_Age
